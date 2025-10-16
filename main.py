553df801bcc2dc4a715295bd9aa94e9db6a5624d
marvellous <120428022+marviecephas@users.noreply.github.com>
2025-10-16 04:26:57 +0100

    Update main.py
    test_train_split + scaling + linear regression

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from google.colab import drive
 import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import r2_score, mean_squared_error
 
 
 drive.mount('/content/drive')
@@ -12,4 +16,20 @@ df = pd.get_dummies(df, columns = ['Platform', 'Genre'], drop_first = True)
 publisher_count = df['Publisher'].value_counts()
 df['Publisher'] = df['Publisher'].map(publisher_count)
 df.rename(columns = {'Publisher' : 'Publisher_encoded'}, inplace = True)
-print(df.head())
+df.dropna(inplace = True)
+print(df)
+
+X = df.drop(columns = ['EU_Sales', 'JP_Sales', 'Other_Sales', 'Global_Sales', 'Rank'])
+y = df['Global_Sales']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size= 0.2)
+x_scale = StandardScaler()
+y_scale = StandardScaler()
+y_train_reshaped = y_train.values.reshape(-1, 1)
+X_train_scaled = x_scale.fit_transform(X_train)
+y_train_scaled = y_scale.fit_transform(y_train_reshaped)
+model = LinearRegression ()
+model.fit(X_train_scaled, y_train_scaled)
+predictions_reshaped = model.predict(x_scale.transform(X_test)).reshape(-1, 1)
+y_pred = y_scale.inverse_transform(predictions_reshaped)
+print(y_pred)
+print(r2_score(y_test, y_pred.flatten()))
